c58320e41ea5d1db9a3a01494071695f23f3436d
Joseph King <josephwaynek2000@gmail.com>
2026-03-25 23:44:48 -0400

    app.py update

--- app.py
+++ app.py
@@ -242,7 +242,18 @@ def dashboard():
         page="dashboard"
     )
 
+@app.route("/clear-songs")
+def clear_songs():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM songs")
+    conn.commit()
+    cur.close()
+    conn.close()
 
+    return "All songs deleted"
+
+    
 @app.route("/letters")
 def letters():
     if "user" not in session:
